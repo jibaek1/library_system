f29be44f5a9dadcca8f882fbca0a9411cf5a35ab
jibaek1 <tngus000777@gmail.com>
2025-05-19 14:57:30 +0900

    studentDAO구현완료

--- src/dao/StudentDAO.java
+++ src/dao/StudentDAO.java
@@ -11,13 +11,12 @@ public class StudentDAO {
 
     // 새 학생을 데이터베이스에 추가하는 기능
     public void addStudent(Student student) throws SQLException {
-        // 쿼리문....
-        String sql = "INSERT INTO Students (name,Student_ID)" + "VALUES(? , ? ) ";
+        // 1.쿼리문 만들기 및 테스트 (DB에서)
+        String sql = "INSERT INTO students (name , student_id) values (?, ?) ";
         try(Connection conn = DatabaseUtil.getConnection();
             PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1,student.getName());
             pstmt.setString(2,student.getStudentId());
-
             pstmt.executeUpdate();
         }
     }
@@ -27,12 +26,16 @@ public class StudentDAO {
         List<Student> studentList = new ArrayList<>();
         String sql = "SELECT * FROM students ";
         try(Connection conn = DatabaseUtil.getConnection();
-        Statement stmt = conn.createStatement()) {
-            ResultSet rs = stmt.executeQuery(sql);
+            PreparedStatement pstmt = conn.prepareStatement(sql)){
+            ResultSet rs = pstmt.executeQuery(sql);
+
+
             while (rs.next()) {
-                int id = rs.getInt("id");
-                String Name = rs.getString("name");
-                String StudentId = rs.getString("StudentId");
+                Student studentDto = new Student();
+                studentDto.setId(rs.getInt("id"));
+                studentDto.setName(rs.getString("name"));
+                studentDto.setStudentId(rs.getString("StudentId"));
+                studentList.add(studentDto);
             }
         }
 
@@ -44,9 +47,31 @@ public class StudentDAO {
         // 학생이 정확한 학번을 입력하면 Student 객체가 만들어져서 리턴 됨
         // 학생이 잘못된 학번을 입력하면 null 값을 반환함
         // if return new Student();
+        String sql = "INSERT INTO students (name , student_id) values (?, ?) ";
+        try (Connection conn = DatabaseUtil.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, studentId);
+            ResultSet rs = pstmt.executeQuery();
 
-        return null;
+//         //   while (rs.next()) {
+//                Student studentDTO = new Student();
+//                studentDTO.setId(rs.getInt("id"));
+//                studentDTO.setName(rs.getString("name"));
+//                studentDTO.setStudentId(rs.getString("student_id"));
+//
+//                return studentDTO;
+//            }
+//
+            if (rs.next()) {
+                Student studentDTO = new Student();
+                studentDTO.setId(rs.getInt("id"));
+                studentDTO.setName(rs.getString("name"));
+                studentDTO.setStudentId(rs.getString("student_id"));
+
+                return studentDTO;
+            }
+            return null;
+        }
     }
-
 
 }
